840a4c67b0256e8e95bb1778f3c92f651b21afc4
Joshua J. Haller <phone@joshuajhaller.com>
2021-01-18 19:51:16 -0800

    Starter files.

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+// const sessions = require("express-session");
+
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(
+      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
+});
