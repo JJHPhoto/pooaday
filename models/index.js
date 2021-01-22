a98062de33b90e0c25101dd88dd843620a8a8de1
lana <lanabkim@gmail.com>
2021-01-21 20:23:01 -0800

    html layouts with style

--- models/index.js
+++ models/index.js
@@ -9,41 +9,35 @@ var config = require(__dirname + "/../config/config.js");
 var db = {};
 
 if (process.env[config.production.use_env_variable]) {
-  var sequelize = new Sequelize(
-    process.env[config.production.use_env_variable]
-  );
+	var sequelize = new Sequelize(
+		process.env[config.production.use_env_variable]
+	);
 } else {
-  var sequelize = new Sequelize(
-    config.development.database,
-    config.development.username,
-    config.development.password,
-    config.development
-  );
+	var sequelize = new Sequelize(
+		config.development.database,
+		config.development.username,
+		config.development.password,
+		config.development
+	);
 }
 
 fs.readdirSync(__dirname)
-  .filter(function (file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
-  })
-  .forEach(function (file) {
-<<<<<<< HEAD
-    const model = require(path.join(__dirname, file))(
-      sequelize, 
-      Sequelize.DataTypes);
-=======
-  const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
->>>>>>> 061ea53ae6dc7700812898188c070b8b7fc8833f
-  });
+	.filter(function (file) {
+		return (
+			file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+		);
+	})
+	.forEach(function (file) {
+		const model = require(path.join(__dirname, file))(
+			sequelize,
+			Sequelize.DataTypes
+		);
+	});
 
 Object.keys(db).forEach(function (modelName) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+	if (db[modelName].associate) {
+		db[modelName].associate(db);
+	}
 });
 
 db.sequelize = sequelize;
