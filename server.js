1d57c8ad68ff47c9b25e237735f4a0c212cd06b7
David Vargas <davidvargas95@gmail.com>
2021-01-23 11:36:13 -0800

    added passport authentication to server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 // const sessions = require("express-session");
 
+const passport = require("./config/passport");
+
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
@@ -9,12 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(authRoutes, htmlRoutes);
+
 db.sequelize.sync({ force: true }).then(() => {
-	app.listen(PORT, () => {
-		console.log(
-			"==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
-			PORT,
-			PORT
-		);
-	});
+  app.listen(PORT, () => {
+    console.log(
+      "==> Listening on port %s. Visit http://localhost:%s/ in your browser.",
+      PORT,
+      PORT
+    );
+  });
 });
