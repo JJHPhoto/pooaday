f720c63a9ed411407a0087c62d0a7174d6ed4abf
Dilberovicka31 <dilberovicm36@gmail.com>
2021-01-23 11:53:09 -0800

    fixed BM route

--- routes/bm-route.js
+++ routes/bm-route.js
@@ -1,31 +1,28 @@
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const {BM} = require("../models");
-const passport = require("../config/middleware/isAuthenticated")
+const { BM } = require("../models");
+const router = require("express").Router();
 
-module.exports = (app) => {
-    app.get("/api/bm/", isAuthenticated, (req,res) =>{
-        BM.findAll({
-            where: {
-                id: //waiting for demo file from passport, need to rewatch the demo
-            }
-        }).then((BM)=>{
-            res,json(BM);
-        })
-    });
+router.get("/api/bm", isAuthenticated, (req, res) => {
+  BM.findAll({
+    where: {
+      UserId: req.user.id,
+    },
+  }).then((bm) => {
+    res.json(bm);
+  });
+});
 
-    app.post("/api/bm/", isAuthenticated, (req,res) =>{
-        BM.create({
-            date: req.body.date,
-            time: req.body.time,
-            type: req.body.type,
-            amount: req.body.amount,
-            speed: req.body.speed,
-            comfort: req.body.comfort,
+router.post("/api/bm/", isAuthenticated, (req, res) => {
+  BM.create({
+    date: req.body.date,
+    time: req.body.time,
+    type: req.body.type,
+    amount: req.body.amount,
+    speed: req.body.speed,
+    comfort: req.body.comfort,
+  }).then((bm) => {
+    res.json(bm);
+  });
+});
 
-        })
-        .then((BM)=>{
-         res.json(BM);
-        });
-    })
-}
-
+module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use(routes);
+// app.use(routes);
 
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
