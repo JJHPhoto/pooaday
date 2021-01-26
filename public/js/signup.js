462c6f9b822368daf550e765760a8a0f80cffa1e
David Vargas <davidvargas95@gmail.com>
2021-01-25 22:00:16 -0800

    done for tonight

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,24 +2,32 @@ $(document).ready(() => {
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
   const passwordInput = $("input#password-input");
+  const nameInput = $("input#name-input");
+
+  console.log(signUpForm);
+  console.log(emailInput);
 
   signUpForm.on("submit", (event) => {
     event.preventDefault();
     const userData = {
+      name: nameInput.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim(),
     };
+    console.log(userData);
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email || !userData.password || !userData.name) {
       return;
     }
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.name, userData.email, userData.password);
+    nameInput.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
-  function signUpUser(email, password) {
+  function signUpUser(email, password, name) {
     $.post("/api/signup", {
+      name: name,
       email: email,
       password: password,
     })
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,48 +3,48 @@
 
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-const router = require("express").Router();
+// const router = require("express").Router();
 
-// module.exports = function (app) {
-router.get("/", (req, res) => {
-  if (req.user) {
-    res.redirect("/members");
-  }
-  res.render("index");
-  // res.sendFile(path.join(__dirname, "/signup.html"));
-});
+module.exports = function (app) {
+  app.get("/", (req, res) => {
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.render("index");
+    // res.sendFile(path.join(__dirname, "/signup.html"));
+  });
 
-router.get("/signup", (req, res) => {
-  if (req.user) {
-    res.redirect("/");
-  }
-  res.render("login");
-});
+  app.get("/signup", (req, res) => {
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.render("login");
+  });
 
-router.get("/login", (req, res) => {
-  if (req.user) {
-    res.redirect("/");
-  }
-  res.render("login");
-});
+  app.get("/login", (req, res) => {
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.render("login");
+  });
 
-router.get("/members", isAuthenticated, (req, res) => {
-  if (req.user) {
-    res.redirect("/");
-  }
-  res.render("members");
-});
+  app.get("/members", isAuthenticated, (req, res) => {
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.render("members");
+  });
 
-router.get("/bm", isAuthenticated, (req, res) => {
-  if (req.user) {
-    res.redirect("/");
-  }
-  res.render("bm");
-});
+  app.get("/bm", isAuthenticated, (req, res) => {
+    if (req.user) {
+      res.redirect("/");
+    }
+    res.render("bm");
+  });
+};
 
 // router.get("/report", isAuthenticated, (req, res) => {
 //   res.sendFile(path.join(__dirname, "../public/report.html"));
 // });
 
-module.exports = router;
 // };
