c8ef6313c7dd15ca7af956d802f3dd1ad79960ef
David Vargas <davidvargas95@gmail.com>
2021-01-25 20:56:49 -0800

    commit to pull

--- config/middleware/isAuthenticated.js
+++ config/middleware/isAuthenticated.js
@@ -7,5 +7,5 @@ module.exports = function (req, res, next) {
   // return res.status(500).json({success: false});
 
   // If the user isn't logged in, redirect them to the login page
-  return res.redirect("/");
+  return res.redirect("/login");
 };
