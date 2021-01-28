c74168da63e75d615e9e832c5d9fba25016c0e5c
Dilberovicka31 <dilberovicm36@gmail.com>
2021-01-27 21:26:19 -0800

    created update route

--- public/js/bm.js
+++ public/js/bm.js
@@ -1,15 +1,15 @@
 $("#BM-add-btn").on("click", () => {
-    const date = $("#datepicker").val();
-     if(!date){
-         return false;
-     }
-     const time = $("#timepicker").val();
-     const style = $("#styleRange").val();
-     
-     const amount = $("#amountRange").val();
-     const speed = $("#speedRange").val();
-     const comfort = $(".comfort:checked").val();
-     console.log(comfort);
+  const date = $("#datepicker").val();
+  if (!date) {
+    return false;
+  }
+  const time = $("#timepicker").val();
+  const style = $("#styleRange").val();
+
+  const amount = $("#amountRange").val();
+  const speed = $("#speedRange").val();
+  const comfort = $(".comfort:checked").val();
+  console.log(comfort);
   $.ajax({
     method: "POST",
     url: "/api/bm",
@@ -21,7 +21,24 @@ $("#BM-add-btn").on("click", () => {
       speed: speed,
       comfort: comfort,
     },
-  }).then((res)=>{
+  }).then((res) => {
     console.log(res);
-  })
+  });
+});
+
+$("#BM-edit-btn").on("click", (bm) => {
+  const date = $("#datepicker").val();
+  const time = $("#timepicker").val();
+  const style = $("#styleRange").val();
+  const amount = $("#amountRange").val();
+  const speed = $("#speedRange").val();
+  const comfort = $(".comfort:checked").val();
+
+  $.ajax({
+    method: "PUT",
+    url: "/api/bm",
+    data: bm,
+  }).then((res) => {
+    return res.json(bm);
+  });
 });
