380691f26eb55e9c259a6e8cc90859ac3bfde610
lana <lanabkim@gmail.com>
2021-01-30 10:29:54 -0800

    adding mia work

--- models/report.js
+++ models/report.js
@@ -1,4 +1,38 @@
-module.exports = function (sequelize, DataTypes) {
-  const Report = sequelize.define("Report", {});
-  return Report;
+module.exports = function (sequelize, DataTypes) {		module.exports = function (sequelize, DataTypes) {
+  const Report = sequelize.define("Report", {		  
+    mood: {		
+      type: DataTypes.STRING,		
+      validate: {		
+      isIn: [["bad", "not bad", "good", "very good", "excellent"]],		
+      },		
+      allowNull: false,		
+    },		
+    water:{		
+      type: DataTypes.STRING,		
+      allowNull: false,		
+    },		
+    food: {		
+      type: DataTypes.STRING,		
+      allowNull: false,		
+    },		
+    activity:{		
+      type: DataTypes.STRING,		
+      validate: {		
+        isIn:[["no activity", "low activity", "moderate activity","vigorous activity" ]],		
+      },		
+      allowNull: false,		
+    },		
+    sleep :{		
+      type: DataTypes.STRING,		
+      validate: {		
+        isIn : [["little", "normal", "a lot"]]		
+      },		
+      allowNull: false,		
+    },		
+    medication: {		
+      type: DataTypes.STRING,		
+      allowNull: false		
+    }		
+  });		
+  return Report;		 
 };
