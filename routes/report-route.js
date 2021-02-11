8e88808d8ce4930890c71b591d4b778b2e6abd0c
Dilberovicka31 <dilberovicm36@gmail.com>
2021-02-10 21:04:18 -0800

    created get and post route for Report

--- routes/report-route.js
+++ routes/report-route.js
@@ -2,14 +2,44 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 const router = require("express").Router();
 const { Report } = require("../models");
 
-// router.get("/api/bm", isAuthenticated, (req, res) => {
-//   BM.findAll({
-//     where: {
-//       UserId: req.user.id,
-//     },
-//   }).then((bm) => {
-//     res.json(bm);
-//   });
-// });
+router.get("/api/members", isAuthenticated, (req, res) => {
+  Report.findAll({
+    where: {
+      UserId: req.user.id,
+    },
+  }).then((result) => {
+    res.json(result);
+  });
+});
 
-// module.exports = router;
+router.get("/myentry", isAuthenticated, (req, res) => {
+	Report.findAll({
+		where: {
+			UserId: req.user.id,
+		},
+	}).then((dbresults) => {
+		let resultsObj = dbresults.map((dbresult) => dbresult.toJSON());
+		let results = { Reports: resultsObj };
+
+		console.log(results, "ressrersresr");
+		///// getting something... getting arrays
+
+		res.render("report", results);
+	});
+});
+router.post("/api/members", isAuthenticated, (req,re)=>{
+    Report.create({
+        mood: req.body.mood,
+        water: req.body.water,
+        food: req.body.food,
+        activity: req.body.activity,
+        sleep: req.body.sleep,
+        medication: req.body.medication
+
+
+    }).then((result)=>{
+        res.json(result);
+    })
+})
+
+module.exports = router;
